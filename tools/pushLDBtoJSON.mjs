ac6e123e85443003837c4bac1a7443ee59b8f5c6
Savantford <36486145+Savantford@users.noreply.github.com>
2025-11-26 18:07:38 -0300

    Updated package-lock.json

--- tools/pushLDBtoJSON.mjs
+++ tools/pushLDBtoJSON.mjs
@@ -30,7 +30,7 @@ for (const pack of packs) {
 function transformName(doc, context) {
   const safeFileName = doc.name.replace(/[^a-zA-Z0-9А-я]/g, "_");
 
-  const prefix = ["Actor", "Item"].includes(context.documentType) ? doc.type : context.documentType;
+  const prefix = ["Actor", "Item", "JournalEntryPage"].includes(context.documentType) ? doc.type : context.documentType;
 
   let name = `${doc.name ? `${prefix}_${safeFileName}_${doc._id}` : doc._id}.${yaml ? "yml" : "json"}`;
   if ( context.folder ) name = path.join(context.folder, name);
