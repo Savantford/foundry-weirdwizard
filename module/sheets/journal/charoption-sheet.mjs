3251790b95aa5cf5c18c3fcc300e2fa7e99f4c81
Savantford <36486145+Savantford@users.noreply.github.com>
2026-04-29 14:22:39 -0300

    Enriched item description in CharOption benefits

--- module/sheets/journal/charoption-sheet.mjs
+++ module/sheets/journal/charoption-sheet.mjs
@@ -140,7 +140,7 @@ export default class WWCharOptionSheet extends WWSheetMixin(JournalEntryPageHand
             uuid: i,
             name: retrieved ? (retrieved.system.magical ? `${retrieved.name} (${i18n('WW.Talent.Magical')})` : retrieved.name) : i18n('WW.CharOption.Unknown'),
             img: retrieved ? retrieved.img : '',
-            description: retrieved ? retrieved.system.description : i18n('WW.CharOption.MissingRef'),
+            description: retrieved ? await TextEditor.enrichHTML(retrieved.system.description, { secrets: retrieved.isOwner, relativeTo: retrieved }) : i18n('WW.CharOption.MissingRef'),
             missing: retrieved ? false : true
           });
         }
