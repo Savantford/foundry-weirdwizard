3a3bb368ded098f9bdf3248e38b5d526afcce6b9
Savantford <36486145+Savantford@users.noreply.github.com>
2026-05-01 17:44:15 -0300

    Fixed BaseEffectModel breaking from the lack of changes

--- module/data/effects/base-effect.mjs
+++ module/data/effects/base-effect.mjs
@@ -1,9 +1,9 @@
 import { makeBooField, makeRequiredStrField, makeUuidStrField, makePosIntField } from "../field-presets.mjs";
 
-export default class BaseEffectModel extends foundry.abstract.TypeDataModel {
+export default class BaseEffectModel extends foundry.data.ActiveEffectTypeDataModel {
   static defineSchema() {
     const fields = foundry.data.fields;
-    
+    console.error()
     const schema = {
       target: makeRequiredStrField('none'),
       trigger: makeRequiredStrField('passive'),
@@ -16,7 +16,10 @@ export default class BaseEffectModel extends foundry.abstract.TypeDataModel {
         autoExpire: makeBooField(true)
       }),
       
-      grantedBy: makeUuidStrField()
+      grantedBy: makeUuidStrField(),
+
+      // Changes
+      changes: super.defineSchema().changes
     }
 
     return schema;
