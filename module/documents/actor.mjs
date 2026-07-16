3f208db02a63048c66909a0b6bb342c506d9630f
BoyBoyMan <Kertes14@gmx.de>
2026-07-16 12:43:42 +0200

    fix speed changes from effects

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -164,6 +164,8 @@ export default class WWActor extends WWDocumentMixin(foundry.documents.Actor) {
 
     // Initialize Speed variables
     this.system.stats.speed.halved = false;
+    // give effects with override something to compare their value to, instead of comparing to null
+    this.system.stats.speed.override = Infinity;
 
     // Initialize dynamic Defense properties
     this.system.stats.defense.armored = 0;
@@ -478,7 +480,9 @@ export default class WWActor extends WWDocumentMixin(foundry.documents.Actor) {
     speed.current = speed.halved ? Math.floor(adjusted / 2) : adjusted;
     
     // Override Speed
-    if (speed.override && speed.current > speed.override) speed.current = speed.override;
+    if (speed.override !== null && speed.override !== undefined && speed.current > speed.override) {
+      speed.current = speed.override;
+    }
   }
 
   /* -------------------------------------------- */
