927d517c8ce2ffa88bb9255bf59954462417c20f
Savantford <36486145+Savantford@users.noreply.github.com>
2026-09-04 13:58:17 -0300

    Replaced sizeDisplayed with sizeFraction in Ancestries

--- module/sheets/journal/charoption-sheet.mjs
+++ module/sheets/journal/charoption-sheet.mjs
@@ -183,7 +183,7 @@ export default class WWCharOptionSheet extends WWSheetMixin(JournalEntryPageHand
       for (const b in context.benefits) {
         const benefit = context.benefits[b];
         
-        benefit.sizeDisplayed = game.weirdwizard.utils.nearestFraction(benefit.stats.sizeNormal);
+        benefit.sizeFraction = game.weirdwizard.utils.nearestFraction(benefit.stats.sizeNormal);
         benefit.sizeTooltip = _loc('WW.Stats.SizeConversionTip');
       }
     }
