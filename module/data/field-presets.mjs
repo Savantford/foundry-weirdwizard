55ab4ad927b53c4f8c7008e81554d92118c80a96
Savantford <36486145+Savantford@users.noreply.github.com>
2026-09-21 16:16:29 -0300

    Refactored field preset options to be an object instead of having multiple args

--- module/data/field-presets.mjs
+++ module/data/field-presets.mjs
@@ -5,93 +5,137 @@ const fields = foundry.data.fields;
 /* -------------------------------------------- */
 
 /* Integer (Min 0) */
-export const makeIntField = (init = 0) => new fields.NumberField({
-  required: true,
-  initial: init,
-  min: 0,
-  nullable: true,
-  integer: true,
-  clean: true
-})
+export const makeIntField = (options={}) => {
+  const { init = 0 } = options;
+
+  return new fields.NumberField({
+    required: true,
+    initial: init,
+    min: 0,
+    nullable: true,
+    integer: true,
+    clean: true
+  })
+}
 
 /* Positive Integer (Min 1) */
-export const makePosIntField = (init = 0) => new fields.NumberField({
-  required: true,
-  initial: init,
-  min: 0,
-  nullable: true,
-  integer: true,
-  clean: true
-})
+export const makePosIntField = (options={}) => {
+  const { init = 1 } = options;
+
+  return new fields.NumberField({
+    required: true,
+    initial: init,
+    min: 1,
+    nullable: true,
+    integer: true,
+    clean: true
+  })
+}
 
 /* Float (Min 0) */
-export const makeFloField = (init = 0) => new fields.NumberField({
-  required: true,
-  initial: init,
-  min: 0,
-  nullable: true,
-  integer: false
-})
+export const makeFloField = (options={}) => {
+  const { init = 0 } = options;
+
+  return new fields.NumberField({
+    required: true,
+    initial: init,
+    min: 0,
+    nullable: true,
+    integer: false
+  })
+}
 
 /* Positive Integer */
-export const makePosNegIntField = (init = 0) => new fields.NumberField({
-  required: true,
-  initial: init,
-  nullable: true,
-  integer: true,
-  clean: true
-})
+export const makePosNegIntField = (options={}) => {
+  const { init = 0 } = options;
+
+  return new fields.NumberField({
+    required: true,
+    initial: init,
+    nullable: true,
+    integer: true,
+    clean: true
+  })
+}
 
 /* Any Number */
-export const makeNumField = (init = 1) => new fields.NumberField({
-  required: true,
-  initial: init,
-  positive: true
-})
+export const makeNumField = (options={}) => {
+  const { init = 1 } = options;
+
+  return new fields.NumberField({
+    required: true,
+    initial: init,
+    positive: true
+  })
+}
 
 /* -------------------------------------------- */
 /*  String Fields                               */
 /* -------------------------------------------- */
 
 /* Regular String */
-export const makeStrField = (init = '') => new fields.StringField({
-  initial: init,
-  blank: true,
-  textSearch: true
-})
+export const makeStrField = (options={}) => {
+  const { init = ''} = options;
+
+  return new fields.StringField({
+    initial: init,
+    blank: true,
+    textSearch: true
+  })
+}
 
 /* ID Reference String (Nullable, cannot be blank) */
-export const makeIdStrField = (init = null) => new fields.StringField({
-  initial: init,
-  nullable: true
-})
+export const makeIdStrField = (options={}) => {
+  const { init = null } = options;
+  
+  return new fields.StringField({
+    initial: init,
+    nullable: true
+  })
+}
 
 /* UUID Reference String (Nullable, cannot be blank) */
-export const makeUuidStrField = (init = null) => new fields.DocumentUUIDField({
-  initial: init,
-  nullable: true,
-  //relative: true - v14 only
-})
+export const makeUuidStrField = (options={}) => {
+  const { init = null } = options;
+
+  return new fields.DocumentUUIDField({
+    initial: init,
+    nullable: true,
+    //relative: true - v14 only
+  })
+}
 
 /* Required String (Cannot be blank; for dropdown and name fields) */
-export const makeRequiredStrField = (init = '') => new fields.StringField({
-  initial: init,
-  blank: false
-})
+export const makeRequiredStrField = (options={}) => {
+  const { init = '' } = options;
+
+  return new fields.StringField({
+    initial: init,
+    blank: false
+  })
+}
 
 /* -------------------------------------------- */
 /*  Other Fields                                */
 /* -------------------------------------------- */
 
-export const makeBooField = (init = false) => new fields.BooleanField ({
-  initial: init
-})
+export const makeBooField = (options={}) => {
+  const { init = false } = options;
+
+  return new fields.BooleanField ({
+    initial: init
+  })
+}
 
 /* Complex Fields */
-export const makeHtmlField = (init = '') => new fields.HTMLField({
-  initial: init,
-  textSearch: true // Allow it to be searched in the Search Bar
-})
+export const makeHtmlField = (options={}) => {
+  const { init = '' } = options;
+
+  return new fields.HTMLField({
+    initial: init,
+    textSearch: true // Allow it to be searched in the Search Bar
+  })
+}
 
 export function makeAttributeField(attribute) {
   const label = 'WW.' + attribute;
